4dc0dedc6d7ff24baa5131a14d6e1e11dc413924
rushi-smb <rushi@smbios.co.in>
2023-03-09 21:36:27 +0530

    Consume API passing Username and password in URL

--- steps/MainPage.xaml.cs
+++ steps/MainPage.xaml.cs
@@ -1,17 +1,53 @@
-﻿using Microsoft.Maui;
+﻿using Android.Media.TV;
+using Microsoft.Maui;
+using Org.Apache.Http.Protocol;
+using steps.MVVM.Models;
 using steps.MVVM.Views;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using static Android.Content.ClipData;
+using System.Text.Json;
+using System.Text;
+using steps.API;
 
 namespace steps;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
+    
+    readonly ILoginRepository _loginRepository = new LoginService();
+
+    public MainPage()
 	{
 		InitializeComponent();
-	}
+
+        #region API Work
+
+        #endregion
+    }
 
     private async void Login_Clicked(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new steps.MVVM.Views.Tab());
+        #region Login using API Credentials
+
+        var userName = UserEntry.Text;
+        var Password = PasswordEntry.Text;
+
+        if (userName == null || Password == null)
+        {
+            await DisplayAlert("Warning", "Please Input UserName & Password", "OK");
+            return;
+        }
+        UserInfo userinfo = await _loginRepository.Login(userName, Password);
+        if (userinfo != null)
+        {
+            await Navigation.PushAsync(new steps.MVVM.Views.Tab());
+        }
+        else 
+        {
+            await DisplayAlert("Warning", "UserName & Password is Incorrect", "OK");
+        }
+
+        #endregion
     }
 }
